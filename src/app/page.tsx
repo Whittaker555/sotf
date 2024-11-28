0b421ee7aad9a3ab53bd73aa1d40fe039510f660
george.whittaker <george.whittaker@awaze.com>
2024-11-28 20:39:47 +0000

    try fixing api url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@
 import { useSession } from "next-auth/react";
 import { signIn, signOut } from "next-auth/react";
 import { useState } from "react";
-import Image from "next/image";
 import { SpotifyPlaylistResponse } from "./models/spotifyplaylistresponse";
 
 export default function Home() {
   const { data: session } = useSession();
   const [playlist, setPlaylist] = useState<SpotifyPlaylistResponse>();
   if (session && !playlist) {
-    fetch("http://localhost:3000/api/spotify")
+      fetch("/api/spotify")
       .then((res) => res.json())
       .then((data) => setPlaylist(data));
   }
@@ -18,29 +17,27 @@ export default function Home() {
       <div className="p-6">
         <p className="text-white font-normal text-xl mt-5 mb-2">Signed In as</p>
         <span className="bold-txt">{session?.user?.name}</span>
-        {playlist?.items
-          .filter((item) => item !== null)
-          .map((item) => (
-            <div
-              key={item?.id}
-              className="flex items-center justify-between w-96 h-24 bg-white rounded-xl p-4 mt-4"
-            >
-              <Image
-                src={item?.images ? item?.images[0].url : ""}
-                alt=""
-                className="w-16 h-16 rounded-lg"
-              />
-              <div className="flex flex-col ml-4">
-                <p className="text-black font-bold">{item?.name}</p>
-                <a
-                  href={item?.external_urls.spotify}
-                  className="text-blue-500 underline"
-                >
-                  Open in Spotify
-                </a>
-              </div>
+        {playlist?.items.filter(item => item !== null).map((item) => (
+          <div
+            key={item?.id}
+            className="flex items-center justify-between w-96 h-24 bg-white rounded-xl p-4 mt-4"
+          >
+            <img
+              src={item?.images ? item?.images[0].url : undefined}
+              alt=""
+              className="w-16 h-16 rounded-lg"
+            />
+            <div className="flex flex-col ml-4">
+              <p className="text-black font-bold">{item?.name}</p>
+              <a
+                href={item?.external_urls.spotify}
+                className="text-blue-500 underline"
+              >
+                Open in Spotify
+              </a>
             </div>
-          ))}
+          </div>
+        ))}
         <p
           className="opacity-70 mt-8 mb-5 underline cursor-pointer"
           onClick={() => signOut()}
