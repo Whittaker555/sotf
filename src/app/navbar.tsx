d9e825f54367c281f8cad6745d33f466999797b6
george.whittaker <george.whittaker@awaze.com>
2024-12-13 16:41:26 +0000

    dont show playlist link if not logged in

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,7 +1,9 @@
 "use client";
+import { useSession } from "next-auth/react";
 import LogIn from "./log-in-button";
 
 export default function NavBar() {
+  const { data: session } = useSession();
   return (
     // <div className="flex justify-between items-center p-4 border-solid border-2 border-gray-800">
     <div className="flex flex-row justify-between items-center p-4 border-solid border-2 border-gray-800">
@@ -16,14 +18,16 @@ export default function NavBar() {
         </button>
       </div>
       <div className="basis-4/12 flex justify-center">
-        <button
-          className="text-white font-bold py-2 px-4 rounded"
-          onClick={() => {
-            window.location.href = "/playlists";
-          }}
-        >
-          Playlists
-        </button>
+        {session && (
+          <button
+            className="text-white font-bold py-2 px-4 rounded"
+            onClick={() => {
+              window.location.href = "/playlists";
+            }}
+          >
+            Playlists
+          </button>
+        )}
       </div>
       <div className="basis-4/12">
         <div className="flex justify-end">
